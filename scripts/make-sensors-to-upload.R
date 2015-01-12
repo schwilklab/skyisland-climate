7832db9122a0eb570c2ec6486e5a727205d6a4ff
Dylan W. Schwilk <dylan@schwilk.org>
2015-01-12 15:10:52 -0600

    Add code to make sensor location shapefile
    But commented it out for now.

--- scripts/make-sensors-to-upload.R
+++ scripts/make-sensors-to-upload.R
@@ -12,3 +12,15 @@ clean.df <- data.frame(name=clean.df$sensor,
 write.csv(clean.df, "../field-sheets/sensors-to-upload.csv", row.names=FALSE)
 # then upload with
 # gpsbabel -i unicsv -o garmin -f sensors-to-upload.csv -F usb:
+
+
+## # And make a shapefile version
+## library(sp)
+## library(rgdal)
+## shp.df <- SpatialPointsDataFrame(coords = clean.df[, c(3,2)], data = clean.df[, c(1,4)])
+## writeOGR(shp.df, "../field-sheets/sensors-shp", layer="sensors-DM", driver="ESRI Shapefile")
+
+## # pretty map of DM
+## clean.df <- clean.df[clean.df$name %in% subset(sensor.master, mtn=="DM")$sensor, ]
+## dmmap <- get_map(location = c(lon=median(clean.df$lon + 0.02), lat=median(clean.df$lat+0.02)), zoom=12)
+## ggmap(dmmap) + geom_point(aes(lon,lat), data=clean.df)
