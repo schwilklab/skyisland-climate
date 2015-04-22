0afd9ba477ad1a8074b75c6eb5cd8e3003e70536
Dylan W. Schwilk <dylan@schwilk.org>
2015-04-22 13:52:49 -0500

    Refactor temperature topo PCA code
    Wrap up PCAs into functions to clean the script up and make it easier to
    run PCAs on every mtn range later.

--- scripts/microclimate-topo-model.R
+++ scripts/microclimate-topo-model.R
@@ -27,6 +27,42 @@ dec2jan <- function(m){
 
 lengthNotNA <- plyr::colwise(function(x) { sum( ! is.na(x))})
 
+# sensor lcoation and topographic data. Make available globally
+sensors <- read.csv("../microclimate/sensors.csv")
+sensors.topo <- read.csv("../microclimate/sensors_topo.csv")
+sensors <- merge(sensors[,c(1,2,3,4,5,6,7)], sensors.topo, by=c("sensor"))
+rm(sensors.topo)
+
+
+# function to run PCA on iBUtton sensor (temperature) data
+runPCA <- function(wdata, minlength=1000, nPC = 5) {
+   # Throw out sensors with less than minlength values. 1000 works for full
+   # data set. But this should be adjusted to keep missing data at <= 10% for
+   # ppca method. TODO
+   df <- wdata[, lengthNotNA(wdata) >= minlength]
+   # Throw out rows in which all elements are NA:
+   df <- df[rowSums(is.na(df[,-1])) != ncol(df[,-1]),]
+   ## Run PCA
+   df.PCA <- pca(df[,-1], nPcs=nPC, method="ppca", center=FALSE)
+   # merge scores back with dates to run time series analysis:
+   scores <- cbind(data.frame(datet=df$datet), as.data.frame(scores(df.PCA)))
+   loadings <- as.data.frame(loadings(df.PCA))
+   loadings$sensor <- rownames(loadings)
+   loadings <- merge(sensors, loadings, by = c("sensor"))
+   return(list(scores = scores, loadings = loadings))
+}
+
+
+getTempPCA <- function(df) {
+   # first step is to get data in a wide format with once columns per sensor
+   df.melted <- melt(df, id.var = c("datet", "sensor"))
+   tmin <- dcast(subset(df.melted, variable=="min"), datet ~ sensor)
+   tmax <- dcast(subset(df.melted, variable=="max"), datet ~ sensor)
+   tmin.PCA <- runPCA(tmin)
+   tmax.PCA <- runPCA(tmax)
+   return(list(tmin=tmin.PCA, tmax=tmax.PCA))
+}
+
 ###############################################################################
 ## Main script starts here
 ###############################################################################
@@ -40,70 +76,27 @@ csv_output <- "../results/tempdata/"
 
 # loads alltemps, temp.daily.sum andtemp.monthly.sum
 source("./load-sensor-data.R")
-
-# read list of sensors stats (elevation, waypoints, etc
-sensors.raw <- read.csv("../microclimate/sensors.csv")
-#sensors.raw$X <- NULL
-
 ## merge summaries with sensor location data
-temp.daily.sum <- merge(temp.daily.sum,sensors.raw)
-
+temp.daily.sum <- merge(temp.daily.sum,sensors)
 
 ## Try walk through on Davis Mtns (DM) data only
-temp.DM <- subset(temp.daily.sum, mtn=="DM")[, 1:4]
+DM.PCA <- getTempPCA(subset(temp.daily.sum, mtn=="DM")[, 1:4])
 
-# melt data
-DM.melted <- melt(temp.DM, id.var = c("datet", "sensor"))
-# cast data back to original wide formats for tmin, tmax and tmean:
-DM.tmin <- dcast(subset(DM.melted, variable=="min"), datet ~ sensor)
-DM.tmax <- dcast(subset(DM.melted, variable=="max"), datet ~ sensor)
+# some exmaple plots on tmin
+plot(DM.PCA$tmin$loadings[, c("elev", "relelev_z", "zdist_valley", "MSD", "PC1", "PC2", "PC3")])
 
-## Now deal with missing values
-lengthNotNA(DM.tmin)
-
-# Throw out sensors with less than 1000 values
-DM.tmin <- DM.tmin[, lengthNotNA(DM.tmin) >= 1000]
-DM.tmax <- DM.tmax[, lengthNotNA(DM.tmax) >= 1000]
-
-# Throw out rows in which all elements are NA:
-DM.tmin <- DM.tmin[rowSums(is.na(DM.tmin[,-1])) != ncol(DM.tmin[,-1]),]
-DM.tmax <- DM.tmax[rowSums(is.na(DM.tmax[,-1])) != ncol(DM.tmax[,-1]),]
+qplot(elev, PC1, data=DM.PCA$tmin$loadings)
+qplot(MSD, PC2, data=DM.PCA$tmin$loadings)
+qplot(relelev_watershed_minmax, PC1, data=DM.PCA$tmin$loadings)
+qplot(relelev_watershed_minmax, PC2, data=DM.PCA$tmin$loadings)
 
 
-## Run PCA
-DM.tmin.PCA <- pca(DM.tmin[,-1], nPcs=5, method="ppca", center=FALSE)
-DM.tmax.PCA <- pca(DM.tmax[,-1], nPcs=5, method="ppca", center=FALSE)
-
-
-# merge scores back with dates to run time series analysis:
-DM.tmin.scores <- cbind(data.frame(datet=DM.tmin$datet), as.data.frame(scores(DM.tmin.PCA)))
-DM.tmax.scores <- cbind(data.frame(datet=DM.tmax$datet), as.data.frame(scores(DM.tmax.PCA)))
-
-
-DM.tmin.loadings <- as.data.frame(loadings(DM.tmin.PCA))
-DM.tmin.loadings$sensor <- rownames(DM.tmin.loadings)
-
-DM.tmax.loadings <- as.data.frame(loadings(DM.tmax.PCA))
-DM.tmax.loadings$sensor <- rownames(DM.tmax.loadings)
-
-# merge loadings with topographical data:
-# read list of sensors stats (elevation, waypoints, etc
-sensors.topo <- read.csv("../microclimate/sensors_topo.csv")
-sensors <- merge(sensors.raw[,c(1,2,3,4,5,6,7)], sensors.topo, by=c("sensor"))
-DM.tmin.loadings <- merge(sensors, DM.tmin.loadings, by = c("sensor"))
-DM.tmax.loadings <- merge(sensors, DM.tmax.loadings, by = c("sensor"))
-
-qplot(elev, PC2, data=DM.tmin.loadings)
-
-
-## Create temp csv files for Helen
-write.csv(DM.tmin.scores, file.path(csv_output, "DM-tmin-scores.csv"), row.names=FALSE)
-write.csv(DM.tmin.loadings, file.path(csv_output, "DM-tmin-loadings.csv"), row.names=FALSE)
-
-write.csv(DM.tmax.scores, file.path(csv_output, "DM-tmax-scores.csv"), row.names=FALSE)
-write.csv(DM.tmax.loadings, file.path(csv_output, "DM-tmax-loadings.csv"), row.names=FALSE)
+## Create temp csv files for Helen (TODO: remove)
+write.csv(DM.PCA$tmin$scores, file.path(csv_output, "DM-tmin-scores.csv"), row.names=FALSE)
+write.csv(DM.PCA$tmin$loadings, file.path(csv_output, "DM-tmin-loadings.csv"), row.names=FALSE)
+write.csv(DM.PCA$tmax$scores, file.path(csv_output, "DM-tmax-scores.csv"), row.names=FALSE)
+write.csv(DM.PCA$tmax$loadings, file.path(csv_output, "DM-tmax-loadings.csv"), row.names=FALSE)
           
-
 
 ## Read in historical data for time series analyses
 source("./wx-data.R")
