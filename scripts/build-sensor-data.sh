6e2fca676092226ea3b131216b552bc74e997cf2
Dylan W. Schwilk <dylan@schwilk.org>
2015-05-14 13:22:29 -0500

    Explicitly call python in shell script
    Try to be more friendly to windoze machines where the shebang mechanism
    does not work.

--- scripts/build-sensor-data.sh
+++ scripts/build-sensor-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # create field sheetsmerge iButton data (only does so if there are new folders)
-./build-merged-ibutton.py -v
+python ./build-merged-ibutton.py -v
 # create rds files (slow!)  only does so if there is new data
 Rscript ./load-sensor-data.R
