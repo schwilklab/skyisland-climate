36b09ba3f16735f591bd77ca73c6c8a5555a7553
Dylan W. Schwilk <dylan@schwilk.org>
2015-07-17 14:33:58 -0500

    random forest script use correct data locations
    1. load PCA data from the microclimate-topo-PCA.R script. Separate csv
    files not needed and should not be in version control
    
    2. Save prediction to tempdata folder, not in version control.

--- scripts/random-forest.R
+++ scripts/random-forest.R
@@ -1,31 +1,49 @@
-
-# example for Helen of how to model the pca loadings
+# random-forest.R
+#
+# Code to fit random forest models
 library(randomForest)
-pc.file <- "DM-tmin-loadings.csv"
-dat <- read.csv(pc.file)
-names(dat)
-##build rf model
-rf1 <- randomForest(PC1 ~  elev +  wetness	+ slope_degrees	+ relelev_l	+ ldist_tovalley	+ ruggedness	+ ldist_ridge	+ zdist_valley	+ radiation	+ relelev_watershed_minmax, data=dat, ntree=2000, mtry=5, importance=T)
-#removed for now bc diff grid size relelev_z  +
+library(raster)
+library(sp)
+library(maptools) # for readASCIIGrid
+
+GIS_DATA_DIR <- "../rf/ascii"
+
+# get the PCA scores and loadings. Function called below returns list of PCA
+# objects, one for each mtn range. Use `force=TRUE` to rerun the PCAs,
+# otherwise the loadings and scores are read from a data object saved in the
+# results/tempdata directory.
+source("./microclimate-topo-PCA.R")
+PCAs <- loadPCAData()
+dat <- PCAs[["DM"]]$tmin$loadings
+
+#names(dat)
+
+## build rf model
+rf1 <- randomForest(PC1 ~  elev +  wetness	+ slope_degrees	+ relelev_l	+
+                           ldist_tovalley	+ ruggedness + ldist_ridge +
+                           zdist_valley + radiation + relelev_watershed_minmax,
+                    data=dat, ntree=2000, mtry=5, importance=T)
+
+# removed for now bc diff grid size relelev_z  +
+
 print(rf1)
 varImpPlot(rf1, type=1)
 partialPlot(rf1, dat, "elev")
+
+
 # now read in rasters, stack them
-library(raster)
-library(sp)
+elev <- readAsciiGrid(file.path(GIS_DATA_DIR, "elev.asc"))
+slope_degrees <- readAsciiGrid(file.path(GIS_DATA_DIR, "slope.asc"))
+wetness <- readAsciiGrid(file.path(GIS_DATA_DIR, "wetness.asc"))
+radiation<- readAsciiGrid(file.path(GIS_DATA_DIR, "radiation.asc"))
+zdist_tovalley<- readAsciiGrid(file.path(GIS_DATA_DIR, "distvalley.asc"))
+ldist_tovalley<- readAsciiGrid(file.path(GIS_DATA_DIR, "ldist_valley.asc"))
+relelev_watershed_minmax <- readAsciiGrid(file.path(GIS_DATA_DIR, "relelev_huc.asc"))
+relelev_l <- readAsciiGrid(file.path(GIS_DATA_DIR, "relelev_l.asc"))
+relelev_z <- readAsciiGrid(file.path(GIS_DATA_DIR, "relelev_z.asc"))
+ldist_ridge<- readAsciiGrid(file.path(GIS_DATA_DIR, "upslope.asc"))
+ruggedness <- readAsciiGrid(file.path(GIS_DATA_DIR, "ruggedness.asc"))
 
-
-elev <- readAsciiGrid("elev.asc")
-slope_degrees <- readAsciiGrid("slope.asc")
-wetness <- readAsciiGrid("wetness.asc")
-radiation<- readAsciiGrid("radiation.asc")
-zdist_tovalley<- readAsciiGrid("distvalley.asc")
-ldist_tovalley<- readAsciiGrid("ldist_valley.asc")
-relelev_watershed_minmax <- readAsciiGrid("relelev_huc.asc")
-relelev_l <- readAsciiGrid("relelev_l.asc")
-relelev_z <- readAsciiGrid("relelev_z.asc")
-ldist_ridge<- readAsciiGrid("upslope.asc")
-ruggedness <- readAsciiGrid("ruggedness.asc")
 r1 <-raster(elev)
 r2<-raster(slope_degrees)
 r3<-raster(wetness)
@@ -43,4 +61,4 @@ names(topostack) <- c("elev", "wetness",	"slope_degrees",	"relelev_l",	"ldist_to
 #removed for now "relelev_z"
 # this makes the predicted loading surface
 predPC1 <- predict(topostack, rf1, type="response")
-writeRaster(predPC1, file="predPC1.tif", overwrite=TRUE)
+writeRaster(predPC1, file=file.path(data_output, "predPC1.tif"), overwrite=TRUE)
