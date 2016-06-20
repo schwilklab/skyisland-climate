8a82bc137e7580eeac0904d84b99ba3be6753e28
Dylan W. Schwilk <dylan@schwilk.org>
2016-06-20 09:30:42 -0500

    retest grids as of June 2016 topo grids

--- scripts/test-gis-grids.R
+++ scripts/test-gis-grids.R
@@ -52,13 +52,12 @@ map_var(DM.topodf, "flow_accum", DM.googlemap)
 map_var(GM.topodf, "flow_accum", GM.googlemap)
 # ok, but high outliers. Probably won't use as direct predictor.
 ggplot(CM.topodf, aes(x=flow_accum)) + geom_histogram()
-# Super high outliers
 quantile(CM.topodf$flow_accum, c(0.1, 0.25, 0.5, 0.95, 1), na.rm=TRUE)
 
 #ldist_ridge2
 map_var(CM.topodf, "ldist_ridge2", CM.googlemap)
 map_var(DM.topodf, "ldist_ridge2", DM.googlemap)
-#map_var(GM.topodf, "ldist_ridge2", GM.googlemap) # does not exist
+map_var(GM.topodf, "ldist_ridge2", GM.googlemap)
 # Basically just shows flow accum so same issues as above
 
 # ldist_ridge
@@ -70,14 +69,12 @@ ggplot(GM.topodf, aes(x=ldist_ridge)) + geom_histogram()
 #
 ggmap(GM.googlemap) + geom_point(aes(x,y), size = 0.001, data=subset(GM.topodf, ldist_ridge==0))
 ggmap(GM.zoommap) + geom_point(aes(x,y), size = 0.001, data=subset(GM.topodf, ldist_ridge==0))
-ggsave("../results/plots/ridge-def-problem-example-GM.png")
+#ggsave("../results/plots/ridge-def-problem-example-GM.png")
 
 # same issue for CM?
 ggmap(CM.zoommap) + geom_point(aes(x,y), size = 0.001, data=subset(CM.topodf, ldist_ridge==0))
-ggsave("../results/plots/ridge-def-problem-example-CM.png")
-# I don't think this ridge definition will be useful. flow accum of zero means
-# the whole landscape! Opened as issue #29
-
+#ggsave("../results/plots/ridge-def-problem-example-CM.png")
+# Ok, looks good now
 
 #ldist_valley2
 map_var(CM.topodf, "ldist_valley2", CM.googlemap)
@@ -105,21 +102,20 @@ map_var(DM.topodf, "msd", DM.googlemap)
 map_var(GM.topodf, "msd", GM.zoommap)
 
 ggplot(CM.topodf, aes(x=msd)) + geom_histogram(binwidth=1)
-# Good, but some really high outliers. That is ok, but we'll need to think
-# about how to sue this variable.
+# Ok, but we'll need to think about how to sue this variable.
 
 # radiation
 map_var(CM.topodf, "radiation", CM.zoommap)
 map_var(DM.topodf, "radiation", DM.googlemap)
 map_var(GM.topodf, "radiation", GM.zoommap)
 
-# I see artifacts.
+# Artefacts fixed!
 # zoom in:
 ggplot(aes(x=x, y=y, fill=radiation), data=GM.topodf) +
     geom_raster() +
     xlim(c(-104.9, -104.8 )) +
     ylim(c(31.9,32))
-ggsave("../results/plots/radiation-gm-zoom-artifacts.png")
+#ggsave("../results/plots/radiation-gm-zoom-artifacts.png")
 
 # is the problem in the elev layer?
 ggplot(aes(x=x, y=y, fill=elev), data=GM.topodf) +
@@ -132,7 +128,8 @@ ggplot(CM.topodf, aes(x=radiation)) + geom_histogram()
 
 
 ggplot(CM.topodf, aes(x=slope, y=radiation, color=elev)) + geom_point()
-ggsave("../results/plots/radiation-issue-CM-example.png")
+#ggsave("../results/plots/radiation-issue-CM-example.png")
+# Looks good now!
 
 # googlemap of radiation
 ggmap(CM.zoommap) + coord_cartesian() +
@@ -141,26 +138,26 @@ ggmap(CM.zoommap) + coord_cartesian() +
                          midpoint=quantile(CM.topodf$radiation, 0.1, na.rm=TRUE))
 #ggsave("../results/plots/radiation_dm_map.png")
 
-# relelev_l
-map_var(CM.topodf, "relelev_l", CM.zoommap)
-map_var(DM.topodf, "relelev_l", DM.googlemap)
-map_var(GM.topodf, "relelev_l", GM.zoommap)
+# relelev_l # deleted
+#map_var(CM.topodf, "relelev_l", CM.zoommap)
+#map_var(DM.topodf, "relelev_l", DM.googlemap)
+#map_var(GM.topodf, "relelev_l", GM.zoommap)
 # Can't trust this as it depends upon the ridge definition (issue #29)
 
 
-# relev_shed
-map_var(CM.topodf, "relelev_shed", CM.googlemap)
-map_var(DM.topodf, "relelev_shed", DM.googlemap)
-map_var(GM.topodf, "relelev_shed", GM.googlemap)
-ggplot(CM.topodf, aes(x=relelev_shed)) + geom_histogram()
+# relev_shed deleted
+#map_var(CM.topodf, "relelev_shed", CM.googlemap)
+#map_var(DM.topodf, "relelev_shed", DM.googlemap)
+#map_var(GM.topodf, "relelev_shed", GM.googlemap)
+#ggplot(CM.topodf, aes(x=relelev_shed)) + geom_histogram()
 # No good, we can't use these arbitrary huc "watersheds" they are not
 # continuous. Opened issue #32
 
 
-# relelev_z
-map_var(CM.topodf, "relelev_z", CM.zoommap)
-map_var(DM.topodf, "relelev_z", DM.googlemap)
-map_var(GM.topodf, "relelev_z", GM.googlemap)
+# relelev_z  # It hought we wanted this one?
+#map_var(CM.topodf, "relelev_z", CM.zoommap)
+#map_var(DM.topodf, "relelev_z", DM.googlemap)
+#map_var(GM.topodf, "relelev_z", GM.googlemap)
 # So affected by issue #29
  
 
@@ -178,7 +175,7 @@ map_var(DM.topodf, "zdist_ridge", DM.googlemap)
 map_var(GM.topodf, "zdist_ridge", GM.zoommap)
 
 ggplot(CM.topodf, aes(x=zdist_ridge)) + geom_histogram()
-# suffers from issue #29
+# Looks good
 
 # zdist_valley
 map_var(CM.topodf, "zdist_valley", CM.zoommap)
@@ -186,13 +183,12 @@ map_var(DM.topodf, "zdist_valley", DM.googlemap)
 map_var(GM.topodf, "zdist_valley", GM.zoommap)
 
 ggplot(CM.topodf, aes(x=zdist_valley)) + geom_histogram()
-# I don't understand this definition. I think negative numbers mean point is
-# ABOVE a valley. Positive mean point is below? That is possible because
-# vallye's end at map edges, so really we'd want to define all of those as NA,
-# I think since a point cannot really be below the nearest valley.
+# Ok
 
 #In fact, I don't understand z_valley upon which this is based:
 # z_valley
-map_var(CM.topodf, "z_valley", CM.zoommap)
-map_var(DM.topodf, "z_valley", DM.googlemap)
-map_var(GM.topodf, "z_valley", GM.zoommap)
+
+# z_valley gone
+#map_var(CM.topodf, "z_valley", CM.zoommap)
+#map_var(DM.topodf, "z_valley", DM.googlemap)
+#map_var(GM.topodf, "z_valley", GM.zoommap)
