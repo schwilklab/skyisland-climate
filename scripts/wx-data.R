12a50b9ef96bbdfcf6b1df50bed7c27552020fb0
Dylan W. Schwilk <dylan@schwilk.org>
2016-06-22 14:55:56 -0500

    Remove unneeded package

--- scripts/wx-data.R
+++ scripts/wx-data.R
@@ -14,15 +14,6 @@
 ###############################################################################
 ## packages and constants
 
-library(repmis) # for reading data from dropbox
-## Note: getting rJava working took some setting of LD paths see
-## http://stackoverflow.com/questions/12872699/error-unable-to-load-installed-packages-just-now/25932828#25932828.
-## My solution, create file /etc/ld.so.conf.d/java.conf with the following
-## lines:
-## /usr/lib/jvm/default-java/jre/lib/amd64
-## /usr/lib/jvm/default-java/jre/lib/amd64/server
-## Then run sudo ldconfig
-
 # historical data location
 hist_data_file <- "https://www.dropbox.com/s/2nx1kuyuswhd984/sky-island-historical-wx-data.csv?raw=1"
 #hist_data_file <- "../wx-station-data/sky-island-historical-wx-data.csv"
