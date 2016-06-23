545fee06c8dc8582d107a0a64cc564353728bb31
Dylan W. Schwilk <dylan@schwilk.org>
2016-06-23 12:08:51 -0500

    Use AICc to select best temporal prediction model
    At each mtn, variable and PC#. I had to put this in one big loop rather
    than create nice functions because of ugly environment issues with model
    update in the MuMIn get.models() function. See
    https://bugs.r-project.org/bugzilla3/show_bug.cgi?id=1861

--- scripts/predict-temporal.R
+++ scripts/predict-temporal.R
@@ -1,43 +1,39 @@
 
 
 library(dplyr)
-#library(MuMIn)
+library(MuMIn)
 
 TEMPO_RES_DIR <- "../results/tempo_mod_results/"
 
 source("wx-data.R") # provides hist_wx_data data frame
-
 source("./microclimate-topo-PCA.R")  # must be run at some point
 
-
-fitTemporalModelRunDiagnostics <- function(mtn, thevar) {
-  # split and redirect output
-  hist_data <- filter(hist_wx_data, MTN==mtn)
-  df <- PCAs[[mtn]][[thevar]]$scores %>% inner_join(hist_data)
-  sink(file = file.path(TEMPO_RES_DIR, paste(mtn, "_", thevar, ".txt", sep="")),
-       append = FALSE, split = TRUE)
-
-  # PC1
-  mod1 <- lm(PC1 ~ TMIN + TMAX + PRCP + TMIN:PRCP + TMAX:PRCP,  data = df, na.action = "na.fail")
-  print(summary(mod1))
-  print(anova(mod1))
-  PC1.predicted <- predict(mod1, hist_data)
-
-  # PC2
-  mod2 <- lm(PC1 ~ TMIN + TMAX + PRCP + TMIN:PRCP + TMAX:PRCP,  data = df)
-  print(summary(mod2))
-  print(anova(mod2))
-  PC2.predicted <- predict(mod2, hist_data)
-  sink(NULL)
-  return(data.frame(datet=hist_data$datet, PC1=PC1.predicted, PC2=PC2.predicted))
-}
-
+PC_AXES <- c("PC1", "PC2", "PC3")
 
 score.predictions <- list()
 for (mtn in c("CM", "DM", "GM")) {
   score.predictions[[mtn]] <- list()
   for (v in c("tmin", "tmax")) {
-      score.predictions[[mtn]][[v]] <- fitTemporalModelRunDiagnostics(mtn, v)
+      hist_data <- filter(hist_wx_data, MTN==mtn)
+      thedf <- PCAs[[mtn]][[v]]$scores %>% inner_join(hist_data)
+      sink(file = file.path(TEMPO_RES_DIR, paste(mtn, "_", v, ".txt", sep="")),
+           append = FALSE, split = TRUE)
+
+      predictions <- list()
+      for (a in PC_AXES) {
+        print(paste(mtn, v, a))
+        modstr <- paste(a,  "~ TMIN + TMAX + PRCP + TMIN:PRCP + TMAX:PRCP")
+        fullmod <- lm(modstr,  data = thedf, na.action = "na.fail")
+        allmods <- dredge(fullmod) # MuMIn function
+        print(allmods)
+        bmod <- get.models(allmods, subset = 1)[[1]]
+        print(summary(bmod))
+        print(anova(bmod))
+        predictions[[a]] <- predict(bmod, hist_data)
+      }
+      predictions$datet <- hist_data$datet
+      score.predictions[[mtn]][[v]] <- do.call(cbind, predictions)
   }
+  sink(NULL)
 }
 
